8d857d4632eb1cd94b4e61b86be235aae7adacbf
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2023-01-30 11:33:22 -0300

    added new logos

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ export { Theme } from "./Themes/ThemesTypes";
 export { parseTheme } from "./Themes/parseTheme";
 export { natura_v2_light_full_theme } from "./natura_v2_mock_theme/theme_natura_v2_mock_light";
 export { natura_v2_dark_full_theme } from "./natura_v2_mock_theme/theme_natura_v2_mock_dark";
-export { consultoriaDeBeleza_light_full_theme } from "./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_light";
-export { consultoriaDeBeleza_dark_full_theme } from "./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_dark";
-
+export {
+  consultoriaDeBeleza_light_full_theme,
+  consultoriaDeBeleza_light_full_theme_pt,
+} from "./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_light";
+export {
+  consultoriaDeBeleza_dark_full_theme,
+  consultoriaDeBeleza_dark_full_theme_pt,
+} from "./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_dark";
