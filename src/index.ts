f8aba72f248c54e825b75e2f94be26f35a81d9bb
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2023-01-30 08:49:39 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { natura_v2_light_full_theme } from "./natura_v2_mock_theme/theme_natura_
 export { natura_v2_dark_full_theme } from "./natura_v2_mock_theme/theme_natura_v2_mock_dark";
 export { consultoriaDeBeleza_light_full_theme } from "./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_light";
 export { consultoriaDeBeleza_dark_full_theme } from "./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_dark";
+
