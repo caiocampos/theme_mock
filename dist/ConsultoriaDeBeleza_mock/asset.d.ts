d645298540e3a0b073256447c80913d94dec8089
caiocampos <20524962+caiocampos@users.noreply.github.com>
2023-01-30 14:33:56 +0000

    Deploying to build from @ caiocampos/theme_mock@8d857d4632eb1cd94b4e61b86be235aae7adacbf 🚀

---
+++ dist/ConsultoriaDeBeleza_mock/asset.d.ts
@@ -0,0 +1,77 @@
+export declare const asset_pt: {
+    font: {
+        file: {
+            display: string;
+            headline: string;
+            body: {
+                regular: string;
+                bold: string;
+            };
+        };
+    };
+    brand: {
+        neutral: {
+            a: {
+                file: string;
+                width: number;
+                height: number;
+            };
+            b: {
+                file: string;
+                width: number;
+                height: number;
+            };
+        };
+        custom: {
+            a: {
+                file: string;
+                width: number;
+                height: number;
+            };
+            b: {
+                file: string;
+                width: number;
+                height: number;
+            };
+        };
+    };
+};
+export declare const asset_es: {
+    font: {
+        file: {
+            display: string;
+            headline: string;
+            body: {
+                regular: string;
+                bold: string;
+            };
+        };
+    };
+    brand: {
+        neutral: {
+            a: {
+                file: string;
+                width: number;
+                height: number;
+            };
+            b: {
+                file: string;
+                width: number;
+                height: number;
+            };
+        };
+        custom: {
+            a: {
+                file: string;
+                width: number;
+                height: number;
+            };
+            b: {
+                file: string;
+                width: number;
+                height: number;
+            };
+        };
+    };
+};
+//# sourceMappingURL=asset.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.consultoriaDeBeleza_dark_full_theme = exports.consultoriaDeBeleza_light_full_theme = exports.natura_v2_dark_full_theme = exports.natura_v2_light_full_theme = exports.parseTheme = void 0;
+exports.consultoriaDeBeleza_dark_full_theme_pt = exports.consultoriaDeBeleza_dark_full_theme = exports.consultoriaDeBeleza_light_full_theme_pt = exports.consultoriaDeBeleza_light_full_theme = exports.natura_v2_dark_full_theme = exports.natura_v2_light_full_theme = exports.parseTheme = void 0;
 var parseTheme_1 = require("./Themes/parseTheme");
 Object.defineProperty(exports, "parseTheme", { enumerable: true, get: function () { return parseTheme_1.parseTheme; } });
 var theme_natura_v2_mock_light_1 = require("./natura_v2_mock_theme/theme_natura_v2_mock_light");
@@ -9,6 +9,8 @@ var theme_natura_v2_mock_dark_1 = require("./natura_v2_mock_theme/theme_natura_v
 Object.defineProperty(exports, "natura_v2_dark_full_theme", { enumerable: true, get: function () { return theme_natura_v2_mock_dark_1.natura_v2_dark_full_theme; } });
 var theme_consultoriaDeBeleza_mock_light_1 = require("./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_light");
 Object.defineProperty(exports, "consultoriaDeBeleza_light_full_theme", { enumerable: true, get: function () { return theme_consultoriaDeBeleza_mock_light_1.consultoriaDeBeleza_light_full_theme; } });
+Object.defineProperty(exports, "consultoriaDeBeleza_light_full_theme_pt", { enumerable: true, get: function () { return theme_consultoriaDeBeleza_mock_light_1.consultoriaDeBeleza_light_full_theme_pt; } });
 var theme_consultoriaDeBeleza_mock_dark_1 = require("./ConsultoriaDeBeleza_mock/theme_consultoriaDeBeleza_mock_dark");
 Object.defineProperty(exports, "consultoriaDeBeleza_dark_full_theme", { enumerable: true, get: function () { return theme_consultoriaDeBeleza_mock_dark_1.consultoriaDeBeleza_dark_full_theme; } });
+Object.defineProperty(exports, "consultoriaDeBeleza_dark_full_theme_pt", { enumerable: true, get: function () { return theme_consultoriaDeBeleza_mock_dark_1.consultoriaDeBeleza_dark_full_theme_pt; } });
 //# sourceMappingURL=index.js.map
